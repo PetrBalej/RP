5700aa8e05955cb22cabef365e3dbcb2f33edecb
Petr Balej <petr@balej.net>
2023-04-02 16:00:52 +0200

    oprava

--- R/ndop.R
+++ R/ndop.R
@@ -46,7 +46,7 @@ SPH_STAT.source <- st_read(paste0(path.data, "cuzk/SPH_SHP_WGS84/WGS84/SPH_STAT.
 ############
 # settings
 ############
-ndop.fs <- list("months" = c(4:6), "years" = c(2019:2022), "precision" = 1000, "version" = "v1")
+ndop.fs <- list("months" = c(4:6), "years" = c(2019:2022), "precision" = 1000, "version" = "v1", "authorsFilter" = c(0))
 
 
 st_crs(SPH_STAT.source) <- 4326
@@ -64,9 +64,9 @@ ndop_ugc <-
       DATUM_DO = col_date("%Y%m%d"),
       DATUM_OD = col_date("%Y%m%d"),
       ZDROJ = "c", SITMAP = "i", X = "d", Y = "d", DAT_SADA = "c",
-      CXPRESNOST = "d", PROJEKT = "c", KAT_TAX = "f", DRUH = "f",
+      CXPRESNOST = "d", PROJEKT = "c", KAT_TAX = "c", DRUH = "c",
       GARANCE = "c", VALIDACE = "c", ID_NALEZ = "n", NEGATIV = "i",
-      VEROH = "i", AUTOR = "f",
+      VEROH = "i", AUTOR = "c",
       UMIST_NAL = "c"
     )
 
@@ -209,7 +209,8 @@ ndop.POLE.join <- as_tibble(ndop.POLE %>% dplyr::select(POLE, ID_NALEZ)) %>% dpl
 
 ndop %<>% left_join(ndop.POLE.join, by = "ID_NALEZ")
 
-ndop %<>% filter(AUTOR0 == 0) # dálě pracuju jen s neproblematickými názvy autorů
+ndop %<>% filter(AUTOR0 %in% ndop.fs$authorsFilter) # dálě pracuju jen s neproblematickými názvy autorů
+ndop.POLE %<>% filter(AUTOR0 %in% ndop.fs$authorsFilter) # dálě pracuju jen s neproblematickými názvy autorů
 nrow(ndop) # 425933
 
 saveRDS(ndop, paste0(path.ndop, "ndop.rds"))
@@ -222,6 +223,9 @@ ndop.stat <- as_tibble(ndop) %>% dplyr::select(-geometry) # pro výpočty je ryc
 ID_NALEZ.total <- nrow(ndop.stat)
 DRUH.total <- length(unique(unlist((ndop.stat$DRUH))))
 POLE.total <- length(unique(unlist((ndop.stat$POLE))))
+AUTOR.total <- length(unique(unlist((ndop.stat$AUTOR))))
+
+### podle autora
 ndop.stat.res <- ndop.stat %>%
   group_by(AUTOR) %>%
   summarise(
@@ -237,15 +241,33 @@ ndop.stat.res <- ndop.stat %>%
 saveRDS(ndop.stat.res, paste0(path.ndop, "ndop.stat.res.rds"))
 write.csv(ndop.stat.res, file = paste0(path.ndop, "ndop.stat.res.csv"), row.names = FALSE)
 
+### podle druhu
+ndop.stat.res.sp <- ndop.stat %>%
+  group_by(DRUH) %>%
+  summarise(
+    ID_NALEZ.n = n_distinct(ID_NALEZ),
+    ID_NALEZ.pct = n_distinct(ID_NALEZ) / ID_NALEZ.total * 100,
+    AUTOR.n = n_distinct(AUTOR),
+    AUTOR.pct = n_distinct(AUTOR) / AUTOR.total * 100,
+    POLE.n = n_distinct(POLE),
+    POLE.pct = n_distinct(POLE) / POLE.total * 100
+  ) %>%
+  arrange(desc(ID_NALEZ.n))
+
+saveRDS(ndop.stat.res.sp, paste0(path.ndop, "ndop.stat.res.sp.rds"))
+write.csv(ndop.stat.res.sp, file = paste0(path.ndop, "ndop.stat.res.sp.csv"), row.names = FALSE)
+
+
+
 #
-# generování přehledových grafů
+# generování přehledových grafů (AUTOR)
 #
 options(scipen = 999)
 ndop.stat.res.boxplots <- ndop.stat.res %>% dplyr::select(-AUTOR)
 
 # nálezy
 ndop.stat.res.boxplots %<>% arrange(desc(ID_NALEZ.n))
-png(paste0(path.ndop, "observers-ID_NALEZ.png"), width = 500, height = 500)
+png(paste0(path.ndop, "AUTOR-observers-ID_NALEZ.png"), width = 520, height = 500)
 par(mar = c(4, 4, 2, 4))
 plot(ndop.stat.res.boxplots$ID_NALEZ.n, log = "x", main = "observers contribution to NDOP by ID_NALEZ", xlab = "observers ordered by activity (most -> less ID_NALEZ) [log10]", ylab = "sum ID_NALEZ")
 grid(NULL, NULL, lty = 6)
@@ -258,7 +280,7 @@ dev.off()
 
 # nálezy cumsum
 ndop.stat.res.boxplots %<>% arrange(desc(ID_NALEZ.n))
-png(paste0(path.ndop, "observers-ID_NALEZ-cumsum.png"), width = 500, height = 500)
+png(paste0(path.ndop, "AUTOR-observers-ID_NALEZ-cumsum.png"), width = 520, height = 500)
 par(mar = c(4, 4, 2, 4))
 plot(cumsum(ndop.stat.res.boxplots$ID_NALEZ.n), log = "x", main = "observers contribution to NDOP by ID_NALEZ", xlab = "observers ordered by activity (most -> less ID_NALEZ) [log10]", ylab = "cumsum ID_NALEZ")
 grid(NULL, NULL, lty = 6)
@@ -271,7 +293,7 @@ dev.off()
 
 
 # druhy (0)
-png(paste0(path.ndop, "observers-DRUH-ordered-ID_NALEZ.png"), width = 500, height = 500)
+png(paste0(path.ndop, "AUTOR-observers-DRUH-ordered-ID_NALEZ.png"), width = 520, height = 500)
 par(mar = c(4, 4, 2, 4))
 plot(ndop.stat.res.boxplots$DRUH.n, log = "x", main = "observers contribution to NDOP by DRUH", xlab = "observers ordered by activity (most -> less ID_NALEZ) [log10]", ylab = "sum DRUH")
 grid(NULL, NULL, lty = 6)
@@ -284,7 +306,7 @@ dev.off()
 
 # druhy
 ndop.stat.res.boxplots %<>% arrange(desc(DRUH.n))
-png(paste0(path.ndop, "observers-DRUH.png"), width = 500, height = 500)
+png(paste0(path.ndop, "AUTOR-observers-DRUH.png"), width = 520, height = 500)
 par(mar = c(4, 4, 2, 4))
 plot(ndop.stat.res.boxplots$DRUH.n, log = "x", main = "observers contribution to NDOP by DRUH", xlab = "observers ordered by activity (most -> less DRUH) [log10]", ylab = "sum DRUH")
 grid(NULL, NULL, lty = 6)
@@ -297,7 +319,7 @@ dev.off()
 
 # pole
 ndop.stat.res.boxplots %<>% arrange(desc(POLE.n))
-png(paste0(path.ndop, "observers-POLE.png"), width = 500, height = 500)
+png(paste0(path.ndop, "AUTOR-observers-POLE.png"), width = 520, height = 500)
 par(mar = c(4, 4, 2, 4))
 plot(ndop.stat.res.boxplots$POLE.n, log = "x", main = "observers contribution to NDOP by POLE", xlab = "observers ordered by activity (most -> less POLE) [log10]", ylab = "sum POLE")
 grid(NULL, NULL, lty = 6)
@@ -309,8 +331,83 @@ mtext("sum POLE [%]", side = 4, line = 3)
 dev.off()
 
 
+
+#
+# generování přehledových grafů (DRUH) (! šlo by v cyklu hromadně...)
+#
+options(scipen = 999)
+ndop.stat.res.boxplots.sp <- ndop.stat.res.sp %>% dplyr::select(-DRUH)
+
+# nálezy
+ndop.stat.res.boxplots.sp %<>% arrange(desc(ID_NALEZ.n))
+png(paste0(path.ndop, "DRUH-observers-ID_NALEZ.png"), width = 520, height = 500)
+par(mar = c(4, 4, 2, 4))
+plot(ndop.stat.res.boxplots.sp$ID_NALEZ.n, log = "x", main = "species contribution to NDOP by ID_NALEZ", xlab = "species ordered by activity (most -> less ID_NALEZ) [log10]", ylab = "sum ID_NALEZ")
+grid(NULL, NULL, lty = 6)
+par(new = TRUE)
+plot(ndop.stat.res.boxplots.sp$ID_NALEZ.pct, log = "x", axes = FALSE, xlab = "", ylab = "") # negeneruju popisky os
+grid(NULL, NULL, lty = 6)
+axis(side = 4, at = pretty(range(ndop.stat.res.boxplots.sp$ID_NALEZ.pct))) # sekundární y osa
+mtext("sum ID_NALEZ [%]", side = 4, line = 3)
+dev.off()
+
+# nálezy cumsum
+ndop.stat.res.boxplots.sp %<>% arrange(desc(ID_NALEZ.n))
+png(paste0(path.ndop, "DRUH-observers-ID_NALEZ-cumsum.png"), width = 520, height = 500)
+par(mar = c(4, 4, 2, 4))
+plot(cumsum(ndop.stat.res.boxplots.sp$ID_NALEZ.n), log = "x", main = "species contribution to NDOP by ID_NALEZ", xlab = "species ordered by activity (most -> less ID_NALEZ) [log10]", ylab = "cumsum ID_NALEZ")
+grid(NULL, NULL, lty = 6)
+par(new = TRUE)
+plot(cumsum(ndop.stat.res.boxplots.sp$ID_NALEZ.pct), log = "x", axes = FALSE, xlab = "", ylab = "") # negeneruju popisky os
+grid(NULL, NULL, lty = 6)
+axis(side = 4, at = pretty(range(cumsum(ndop.stat.res.boxplots.sp$ID_NALEZ.pct)))) # sekundární y osa
+mtext("cumsum ID_NALEZ [%]", side = 4, line = 3)
+dev.off()
+
+
+# autoři (0)
+png(paste0(path.ndop, "DRUH-observers-AUTOR-ordered-ID_NALEZ.png"), width = 520, height = 500)
+par(mar = c(4, 4, 2, 4))
+plot(ndop.stat.res.boxplots.sp$AUTOR.n, log = "x", main = "species contribution to NDOP by AUTOR", xlab = "species ordered by activity (most -> less ID_NALEZ) [log10]", ylab = "sum AUTOR")
+grid(NULL, NULL, lty = 6)
+par(new = TRUE)
+plot(ndop.stat.res.boxplots.sp$AUTOR.pct, log = "x", axes = FALSE, xlab = "", ylab = "") # negeneruju popisky os
+grid(NULL, NULL, lty = 6)
+axis(side = 4, at = pretty(range(ndop.stat.res.boxplots.sp$AUTOR.pct))) # sekundární y osa
+mtext("sum AUTOR [%]", side = 4, line = 3)
+dev.off()
+
+# autoři
+ndop.stat.res.boxplots.sp %<>% arrange(desc(AUTOR.n))
+png(paste0(path.ndop, "DRUH-observers-AUTOR.png"), width = 520, height = 500)
+par(mar = c(4, 4, 2, 4))
+plot(ndop.stat.res.boxplots.sp$AUTOR.n, log = "x", main = "species contribution to NDOP by DRUH", xlab = "species ordered by activity (most -> less AUTOR) [log10]", ylab = "sum AUTOR")
+grid(NULL, NULL, lty = 6)
+par(new = TRUE)
+plot(ndop.stat.res.boxplots.sp$AUTOR.pct, log = "x", axes = FALSE, xlab = "", ylab = "") # negeneruju popisky os
+grid(NULL, NULL, lty = 6)
+axis(side = 4, at = pretty(range(ndop.stat.res.boxplots.sp$AUTOR.pct))) # sekundární y osa
+mtext("sum AUTOR [%]", side = 4, line = 3)
+dev.off()
+
+# pole
+ndop.stat.res.boxplots.sp %<>% arrange(desc(POLE.n))
+png(paste0(path.ndop, "DRUH-observers-POLE.png"), width = 520, height = 500)
+par(mar = c(4, 4, 2, 4))
+plot(ndop.stat.res.boxplots.sp$POLE.n, log = "x", main = "species contribution to NDOP by POLE", xlab = "species ordered by activity (most -> less POLE) [log10]", ylab = "sum POLE")
+grid(NULL, NULL, lty = 6)
+par(new = TRUE)
+plot(ndop.stat.res.boxplots.sp$POLE.pct, log = "x", axes = FALSE, xlab = "", ylab = "") # negeneruju popisky os
+grid(NULL, NULL, lty = 6)
+axis(side = 4, at = pretty(range(ndop.stat.res.boxplots.sp$POLE.pct))) # sekundární y osa
+mtext("sum POLE [%]", side = 4, line = 3)
+dev.off()
+
+
+#########################
+
 # koreluje počet druhů s počtem nálezů (per autor?)
-png(paste0(path.ndop, "species-occurrences.png"), width = 500)
+png(paste0(path.ndop, "species-occurrences.png"), width = 520, height = 500)
 ndop.stat.res.boxplots %<>% arrange(desc(ID_NALEZ.n))
 plot(ndop.stat.res.boxplots$DRUH.n ~ ndop.stat.res.boxplots$ID_NALEZ.n, log = "x", main = "species ~ occurrences (per observer)", xlab = "occurrences  [log10]", ylab = "species")
 grid(NULL, NULL, lty = 6)
@@ -361,8 +458,11 @@ dev.off()
 
 
 #
-# vizualizace map TGOB (per pixel occs) a presencí
+# vizualizace map SSOS-TGOB (per pixel occs) a presencí
 #
+
+dir.create(paste0(path.ndop, "ssos"), showWarnings = FALSE)
+
 SPH_STAT.source.s <- st_simplify(SPH_STAT.source, preserveTopology = FALSE, dTolerance = 0.001)
 # druhy <- unique(as.vector(unlist(ndop.sf.POLE$DRUH)))
 druhy <- unique(as.vector(unlist(ndop$DRUH)))
@@ -371,28 +471,28 @@ for (druh in druhy) {
   print(druh)
   pres <- ndop.POLE %>% filter(DRUH == druh)
   pres.au <- unique(as.vector(unlist(pres$AUTOR)))
-  tgob <- ndop.POLE %>% filter(AUTOR %in% pres.au)
+  ssos <- ndop.POLE %>% filter(AUTOR %in% pres.au)
   pres.unique <- pres %>%
     group_by(POLE) %>%
     slice_head(n = 1) %>%
     st_centroid()
-  tgob.unique <- tgob %>%
+
+  ssos.unique <- ssos %>%
     group_by(POLE) %>%
     slice_head(n = 1)
 
   # počet (unikátních, zrušit znásobení spoluautory) nálezů na pixel
-  tgob.count <- tgob %>%
+  ssos.count <- ssos %>%
     group_by(POLE) %>%
     mutate(pp = length(unique(ID_NALEZ))) %>%
     slice_head(n = 1) %>%
     st_centroid()
-  length(unique(tgob.count$POLE))
 
 
   df.temp <- data.frame(
     "species" = druh,
     "p" = nrow(pres), "pu" = nrow(pres.unique),
-    "tgob" = nrow(tgob), "tgobu" = nrow(tgob.unique),
+    "ssos" = nrow(ssos), "ssosu" = nrow(ssos.unique),
     "authors" = length(pres.au)
   )
   if (first) {
@@ -402,12 +502,12 @@ for (druh in druhy) {
     df.out %<>% add_row(df.temp)
   }
 
-  pl <- tgob.count %>% dplyr::select(pp)
+  pl <- ssos.count %>% dplyr::select(pp)
 
   ggpl <- ggplot() +
     geom_sf(data = SPH_STAT.source.s, fill = "LightBlue") +
     geom_sf(data = pl, aes(colour = pp), pch = 15, size = 2) +
-    labs(color = "TGOB (log10)", caption = paste0("occs source: AOPK NDOP (years: ", min(ndop.fs$years), "-", max(ndop.fs$years), "; months: ", min(ndop.fs$months), "-", max(ndop.fs$months), ") | author: Petr Balej | WGS84 | grid: KFME (SitMap_2Rad) | generated: ", Sys.Date())) +
+    labs(color = "SSOS-TGOB (log10)", caption = paste0("occs source: AOPK NDOP (years: ", min(ndop.fs$years), "-", max(ndop.fs$years), "; months: ", min(ndop.fs$months), "-", max(ndop.fs$months), ") | author: Petr Balej | WGS84 | grid: KFME (SitMap_2Rad) | generated: ", Sys.Date())) +
     scale_colour_gradient(low = "white", high = "red", trans = "log10") +
     theme_light() +
     geom_sf(data = pres.unique) +
@@ -417,10 +517,10 @@ for (druh in druhy) {
     ) +
     ggtitle(
       label = (druh),
-      subtitle = paste0("TGOB (same species observers occs per pixel) | p:", df.temp$pu, " / bg:", df.temp$tgobu, " / a:", df.temp$authors)
+      subtitle = paste0("SSOS-TGOB (same species observers occs per pixel) | p:", df.temp$pu, " / bg:", df.temp$ssosu, " / autors:", df.temp$authors)
     )
 
-  png(paste0(path.ndop, "tgob/tgob-", str_replace_all(druh, "[\\/\\:]", "_"), ".png"), width = 1200, height = 700)
+  png(paste0(path.ndop, "ssos/", str_replace_all(druh, "[\\/\\:]", "_"), ".png"), width = 1200, height = 700)
 
   print(ggpl)
   dev.off()
